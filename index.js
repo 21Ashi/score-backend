dc605dd20328ce901e6011548d4e7fff003f498c
21Ashi <abdelmajeedashi@gmail.com>
2025-07-28 14:43:33 +0300

    c

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { initFirebase } = require('../firebase');
+const { initFirebase } = require('./config/firebase');
 initFirebase();
 require('dotenv').config();
 const express = require('express');
